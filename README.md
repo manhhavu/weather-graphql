ace39ad623138adf93c0355af7c56fcbdd2b6589
Manh-Ha VU <manhha.vu@traveldoo.com>
2017-01-17 11:15:48 +0100

    Combine current weather and forecasts in same response

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 Demonstrates a GraphQL server proxied to [OpenWeatherMap API](http://openweathermap.org/api)
 
 ## Features
-1. Query [current weather data for one location](https://openweathermap.org/current#one) by specifying city's name or id. 
+1. Query following services:
+    * [current weather data for a location](https://openweathermap.org/current#one)
+    * [5 day/3 hour forecast for a location](https://openweathermap.org/forecast5) 
+2. Combine multiple services in a same GraphQL query, e.g. we can have current weather and forecasts in a same response.
 
 ## How to run
 
@@ -11,23 +14,32 @@ You need to specify a OpenWeatherMap APPID as an environment variable (OWM_ID).
 
 Example request:
 ```
-{  
-    location(cityName: "London") { 
-        id 
-        name 
-        main { 
-            temp 
-            pressure
-            humidity
-            temp_min
-            temp_max
-        } 
-    }  
+{ 
+	location(cityName: "London") { 
+		id 
+		name  
+		now {
+		    main {
+			    temp 
+			    humidity
+			 }
+		} 
+		forecasts  { 
+			dt 
+			dt_txt 
+			main { 
+				temp 
+				humidity 
+			} 
+			weather { 
+				main 
+				description 
+			} 
+		}  
+	} 
 }
 ```
-as **query** parameter in a POST (x-www-form-urlencoded) request sent to [http://localhost:9090/weather](http://localhost:9090/weather). More parameters can be found in **WeatherSchema** class.
-
-
+as **query** parameter in a POST (x-www-form-urlencoded) request sent to [http://localhost:9090/location](http://localhost:9090/location). More technicalIndicators can be found in **WeatherSchema** class.
 
 
 ## Credits
--- src/main/kotlin/com/manhhavu/experiments/graphql/WeatherSchema.kt
+++ src/main/kotlin/com/manhhavu/experiments/graphql/WeatherSchema.kt
@@ -3,6 +3,7 @@ package com.manhhavu.experiments.graphql
 import com.mashape.unirest.http.Unirest
 import graphql.Scalars.*
 import graphql.schema.GraphQLFieldDefinition.newFieldDefinition
+import graphql.schema.GraphQLList
 import graphql.schema.GraphQLObjectType
 import graphql.schema.GraphQLObjectType.newObject
 import graphql.schema.GraphQLSchema
@@ -10,6 +11,8 @@ import graphql.schema.GraphQLSchema
 import java.util.*
 
 class WeatherSchema constructor(val appId: String) {
+    private val endpoint = "http://api.openweathermap.org/data/2.5"
+
     val schema: GraphQLSchema
 
     init {
@@ -17,17 +20,21 @@ class WeatherSchema constructor(val appId: String) {
                 .name("Query")
                 .field(newFieldDefinition()
                         .name("location")
-                        .type(weather(parameters()))
+                        .type(location())
                         .argument { it.name("cityName").type(GraphQLString).defaultValue("London") }
                         .argument { it.name("id").type(GraphQLLong) }
                         .dataFetcher {
                             val query = Optional.ofNullable(it.arguments["id"])
                                     .map { "id=$it" }
                                     .orElse("q=${it.arguments["cityName"]}")
-
-                            Unirest.get("http://api.openweathermap.org/data/2.5/weather?$query&APPID=$appId")
+                            val response = Unirest.get("$endpoint/weather?$query&APPID=$appId")
                                     .header("accept", "application/json")
                                     .asJson().body.`object`.asMap()
+
+                            mapOf("id"   to response["id"],
+                                  "name" to response["name"],
+                                  "now"  to mapOf("main"    to response["main"],
+                                                  "weather" to response["weather"]))
                         })
                 .build()
         schema = GraphQLSchema.newSchema()
@@ -35,9 +42,35 @@ class WeatherSchema constructor(val appId: String) {
                 .build()
     }
 
-    private fun parameters(): GraphQLObjectType.Builder? {
+    private fun now(): GraphQLObjectType.Builder {
         return newObject()
-                .name("Parameters")
+                .name("Now")
+                .field(newFieldDefinition().name("main").type(technicalIndicators()))
+                .field(newFieldDefinition().name("weather").type(GraphQLList(humanlyReadableIndicators().build())))
+    }
+
+    private fun location(): GraphQLObjectType.Builder {
+        return newObject()
+                .name("Location")
+                .field(newFieldDefinition().name("id").type(GraphQLFloat))
+                .field(newFieldDefinition().name("name").type(GraphQLString))
+                .field(newFieldDefinition().name("now").type(now()))
+                .field(newFieldDefinition()
+                        .name("forecasts")
+                        .type(GraphQLList(dayForecast().build()))
+                        .dataFetcher {
+                            val query = "q=London"
+                            val response = Unirest.get("$endpoint/forecast?$query&APPID=$appId")
+                                    .header("accept", "application/json")
+                                    .asJson().body.`object`.asMap()
+
+                            response["list"]
+                        })
+    }
+
+    private fun technicalIndicators(): GraphQLObjectType.Builder {
+        return newObject()
+                .name("TechnicalIndicators")
                 .field(newFieldDefinition().name("temp").type(GraphQLFloat))
                 .field(newFieldDefinition().name("pressure").type(GraphQLFloat))
                 .field(newFieldDefinition().name("humidity").type(GraphQLFloat))
@@ -45,17 +78,21 @@ class WeatherSchema constructor(val appId: String) {
                 .field(newFieldDefinition().name("temp_max").type(GraphQLFloat))
     }
 
-    private fun weather(parameters: GraphQLObjectType.Builder?): GraphQLObjectType.Builder? {
+    private fun humanlyReadableIndicators() : GraphQLObjectType.Builder {
         return newObject()
-                .name("Weather")
-                .field(newFieldDefinition()
-                        .name("id")
-                        .type(GraphQLFloat))
-                .field(newFieldDefinition()
-                        .name("name")
-                        .type(GraphQLString))
-                .field(newFieldDefinition()
-                        .name("main")
-                        .type(parameters))
+                .name("HumanlyReadableIndicators")
+                .field(newFieldDefinition().name("main").type(GraphQLString))
+                .field(newFieldDefinition().name("description").type(GraphQLString))
+                .field(newFieldDefinition().name("icon").type(GraphQLString))
     }
+
+    private fun dayForecast() : GraphQLObjectType.Builder {
+        return newObject()
+                .name("DayForecast")
+                .field(newFieldDefinition().name("dt").type(GraphQLString))
+                .field(newFieldDefinition().name("dt_txt").type(GraphQLString))
+                .field(newFieldDefinition().name("main").type(technicalIndicators()))
+                .field(newFieldDefinition().name("weather").type(GraphQLList(humanlyReadableIndicators().build())))
+    }
+
 }
